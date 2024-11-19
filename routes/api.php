21117b56961c4575e2b0e3f6b59f33bc051243ac
pauromaoluche <pauluci125romao.pedro@hotmail.com>
2024-11-19 11:14:21 -0300

    Validando para que seja feitas apenas request pelo twilio

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\WhatsAppController;
+use App\Http\Middleware\TwilioRequestMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/new_message', [WhatsAppController::class, 'new_message'])->name('new_message');
+Route::post('/new_message', [WhatsAppController::class, 'new_message'])
+->middleware(TwilioRequestMiddleware::class)
+->name('new_message');
